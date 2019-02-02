fbb7a6c1eab223630696f719c9bf5c96cb7c4c09
Kaori <xiaxulizhaiteng@gmail.com>
2019-02-02 15:31:10 +0900

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://pure-citadel-38206.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'https://fierce-springs-45307.herokuapp.com/'}
  config.action_mailer.delivery_method = :letter_opener_web
 end
